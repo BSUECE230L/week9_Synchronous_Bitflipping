9728e1325d6543d487f233a1efe7b8571d16057e
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2024-10-14 16:28:50 -0600

    Update behavioral_d_latch.v

--- behavioral_d_latch.v
+++ behavioral_d_latch.v
@@ -1,1 +1,16 @@
+module behavioral_d_latch(
+    input Set,
+    input Reset,
+    output reg Q,
+    output NotQ
+);
 
+    always @(Set, Reset) begin 
+        if (Set)
+            Q = 1;
+        else if (Reset)
+            Q = 0;
+    end
+    assign NotQ = ~Q; 
+
+endmodule
