4e3c52d53e1b10c4c2c3a2d3c5c1d8df100d2a3f
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2024-10-14 17:28:32 -0600

    Update memory_system.v

--- memory_system.v
+++ memory_system.v
@@ -10,19 +10,35 @@ module memory_system(
     // data and store into the one selected by
     // addr
   byte_memory bit0(
-    
+      demux d0(
+          .data_in(data),
+          .store(store),
+          .addr(latch0)
+      );
   );
 
   byte_memory bit1(
-
+      demux d1(
+          .data_in(data),
+          .store(store),
+          .addr(latch1)
+      );
   );
 
   byte_memory bit2(
-
+      demux d2(
+          .data_in(data),
+          .store(store),
+          .addr(latch2)
+      );
   );
 
   byte_memory bit3(
-
+      demux d3(
+          .data_in(data),
+          .store(store),
+          .addr(latch3)
+      );
   );
 
     // It should then multiplex the output of the
