c6601c86f0411eb9b556b1cfab22040b9b9aca6f
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2024-10-15 09:06:49 -0600

    Update memory_system.v

--- memory_system.v
+++ memory_system.v
@@ -9,40 +9,56 @@ module memory_system(
     // byte_memory, and then demultiplex
     // data and store into the one selected by
     // addr
-  byte_memory bit0(
-      demux d0(
-          .data_in(data),
-          .store(store),
-          .addr(latch0)
-      );
-  );
+    reg[7:0] byte0, byte1, byte2, byte3;
 
-  byte_memory bit1(
-      demux d1(
-          .data_in(data),
-          .store(store),
-          .addr(latch1)
-      );
-  );
+    always @(*) begin
+        if (store) begin
+            case(addr)
+                2'b00: byte0 = data;
+                2'b01: byte1 = data;
+                2'b10: byte2 = data;
+                2'b11: byte3 = data;
+            endcase
+        end
+    end
 
-  byte_memory bit2(
-      demux d2(
-          .data_in(data),
-          .store(store),
-          .addr(latch2)
-      );
-  );
-
-  byte_memory bit3(
-      demux d3(
-          .data_in(data),
-          .store(store),
-          .addr(latch3)
-      );
-  );
-
-    // It should then multiplex the output of the
+     // It should then multiplex the output of the
     // memory specified by addr into the memory
     // output for display on the LEDs
+
+    assign memory = addr;
+
+    
+  // byte_memory bit0(
+  //     demux d0(
+  //         .data_in(data),
+  //         .store(store),
+  //         .addr(latch0)
+  //     );
+  // );
+
+  // byte_memory bit1(
+  //     demux d1(
+  //         .data_in(data),
+  //         .store(store),
+  //         .addr(latch1)
+  //     );
+  // );
+
+  // byte_memory bit2(
+  //     demux d2(
+  //         .data_in(data),
+  //         .store(store),
+  //         .addr(latch2)
+  //     );
+  // );
+
+  // byte_memory bit3(
+  //     demux d3(
+  //         .data_in(data),
+  //         .store(store),
+  //         .addr(latch3)
+  //     );
+  // );
 
 endmodule
